a9e2f7984e57a6ea3edcebdfe5743b197b09ad60
Neuroplastic1 <shafqat.umair@gmail.com>
2020-08-12 17:30:39 -0400

    Path redirected on sign-in

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -33,7 +33,7 @@ class SignIn extends Component {
         message: messages.signInSuccess,
         variant: 'success'
       }))
-      .then(() => history.push('/'))
+      .then(() => history.push('/plans'))
       .catch(error => {
         this.setState({ email: '', password: '' })
         msgAlert({
